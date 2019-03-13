3d1c6d173bd43cd2ad3e4913d9bacc72918d42d3
Apurva Nandan <apurva3000@gmail.com>
2019-03-13 15:25:15 +0200

    custom password env

--- server/fix.sh
+++ server/fix.sh
@@ -13,4 +13,11 @@ cp $tempfile2 /etc/group
 rm -f $tempfile2
 echo "rstudio:x:$(id -u):" >> /etc/group
 
-echo "rstudio-server:$6$OLWwdiLp$uLstyoh.dp5yAWgZqoHUj707hxKlca17PrGFoDKvOlX.QHJVdLBm3eBfG9JF0NKjgxCL8QKTl3xMR/LZJSmgR1:17652:0:99999:7:::" >> /etc/shadow
+tempfile3=$(mktemp)
+sed -e "s/^rstudio.*//g" /etc/shadow > $tempfile3
+cp $tempfile3 /etc/shadow
+rm -f $tempfile3
+echo "rstudio:${RStudioPassword}:17866:0:99999:7:::" >> /etc/shadow
+echo "rstudio-server:${RStudioPassword}:17866:0:99999:7:::" >> /etc/shadow
+
+# echo "rstudio-server:$6$OLWwdiLp$uLstyoh.dp5yAWgZqoHUj707hxKlca17PrGFoDKvOlX.QHJVdLBm3eBfG9JF0NKjgxCL8QKTl3xMR/LZJSmgR1:17652:0:99999:7:::" >> /etc/shadow
