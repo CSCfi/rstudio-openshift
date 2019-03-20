0d1bdd33a8ee7ff51cb6f60022291e6ea5a6b590
juhahu <juha.hulkkonen@gmail.com>
2019-03-20 15:00:09 +0200

    Fixes to make custome user to work

--- server/fix.sh
+++ server/fix.sh
@@ -3,6 +3,7 @@
 echo "Creating passwd entries"
 tempfile=$(mktemp)
 sed -e "s/^rstudio.*//g" /etc/passwd > $tempfile
+sed -e "s/^$USERNAME.*//g" /etc/passwd > $tempfile
 cp $tempfile /etc/passwd
 rm -f $tempfile
 echo "rstudio-server:x:$(id -u):$(id -g)::/home/$USERNAME:" >> /etc/passwd
@@ -11,6 +12,7 @@ echo "$USERNAME:x:$(id -u):$(id -g)::/home/$USERNAME:" >> /etc/passwd
 echo "Creating group entries"
 tempfile2=$(mktemp)
 sed -e "s/^rstudio.*//g" /etc/group > $tempfile2
+sed -e "s/^$USERNAME.*//g" /etc/group > $tempfile2
 cp $tempfile2 /etc/group
 rm -f $tempfile2
 echo "$USERNAME:x:$(id -u):" >> /etc/group
@@ -21,6 +23,7 @@ RPassword=$(cat /tmp/shadow/shadow.crypt)
 echo "Creating shadow entries"
 tempfile3=$(mktemp)
 sed -e "s/^rstudio.*//g" /etc/shadow > $tempfile3
+sed -e "s/^$USERNAME.*//g" /etc/shadow > $tempfile3
 cp $tempfile3 /etc/shadow
 rm -f $tempfile3
 echo "$USERNAME:${RPassword}:17866:0:99999:7:::" >> /etc/shadow
